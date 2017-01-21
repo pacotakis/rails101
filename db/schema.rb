cda0aa23194cfe3b3b299d3bd56285abee895c6a
Yanan Sun <sunyanan99@hotmail.com>
2017-01-21 13:00:15 +0800

    description correction

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20170121032131) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "desctription"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
