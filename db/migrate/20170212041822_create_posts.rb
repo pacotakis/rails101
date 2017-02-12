a62a97e610a231c9f23e0e8e61868daaaaf9c562
Yanan Sun <sunyanan99@hotmail.com>
2017-02-12 18:01:06 +0800

    add post hook user/group relation

---
+++ db/migrate/20170212041822_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :content
+      t.string :text,
+      t.string :group_id
+      t.string :integer,
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
